a260e452beb9c8fc28f7f36a8ac6dd1f248a6ccd
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-07-04 20:10:21 +0100

    Working on questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -10,6 +11,48 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('QuestionsScreen'));
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          questions[0].text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(
+            questions[0].answers[0],
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(
+            questions[0].answers[1],
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(
+            questions[0].answers[2],
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(
+            questions[0].answers[3],
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+    ));
   }
 }
