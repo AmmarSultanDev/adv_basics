ce678f93caf148936c991cc4c9be09135b621409
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-07-09 19:17:32 +0100

    use GoogleFonts

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -33,8 +34,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             Text(
               currentQuestion.text,
               textAlign: TextAlign.center,
-              style: const TextStyle(
-                  color: Colors.white,
+              style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 223, 152, 246),
                   fontWeight: FontWeight.bold,
                   fontSize: 24),
             ),
